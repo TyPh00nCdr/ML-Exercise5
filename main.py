8dfc0c92917489e4b3aa762f4fe61654b4871542
Jan Niklas Hennings <3henning@informatik.uni-hamburg.de>
2019-05-27 10:31:48 +0200

    Load data and draw scatterplot

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import numpy as np
 import sklearn
+import matplotlib.pyplot as plt
 
 
 def main():
-    pass
+    data_circle = np.loadtxt('data/dataCircle.txt')
+    fig, ax = plt.subplots()
+    ax.scatter(*data_circle[:, :2].T, c=data_circle[:, 2])
+    plt.show()
 
 
 if __name__ == '__main__':
