85b0b453980f90e3ab668a39d322f6a503cd3fb4
Jan Niklas Hennings <3henning@informatik.uni-hamburg.de>
2019-05-27 11:17:45 +0200

    Refactor plotting boilerplate into own method

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.ensemble import AdaBoostClassifier
 
 def main():
     data_circle = np.loadtxt('data/dataCircle.txt')
+    global feats, labels
     feats = data_circle[:, :2]
     labels = data_circle[:, 2]
 
@@ -12,21 +13,24 @@ def main():
     ada_boost = AdaBoostClassifier()
     ada_boost.fit(feats, labels)
 
-    # Plot decision boundaries
+    fig, ax = plt.subplots()
+    plot_boundaries(ax, ada_boost)
+
+    # Plot data pointsFs
+    ax.scatter(*feats.T, c=labels)
+    plt.show()
+
+
+def plot_boundaries(ax, clf):
     plot_step = 0.02
     x_min, x_max = feats[:, 0].min() - 0.5, feats[:, 0].max() + 0.5
     y_min, y_max = feats[:, 1].min() - 0.5, feats[:, 1].max() + 0.5
     xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                          np.arange(y_min, y_max, plot_step))
-
-    fig, ax = plt.subplots()
-    Z = ada_boost.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     ax.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
-
-    # Plot data points
-    ax.scatter(*feats.T, c=labels)
-    plt.show()
+    return ax
 
 
 if __name__ == '__main__':
